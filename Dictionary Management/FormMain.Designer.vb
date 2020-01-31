45d04d585bd33e012ff129eb756a3d6dfb2297be
hz1624917200 <hz1624917200@outlook.com>
2020-01-31 10:06:28 +0800

    Add files via upload

---
+++ Dictionary Management/FormMain.Designer.vb
@@ -0,0 +1,304 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormMain
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonReadFile = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.LabelFile = New System.Windows.Forms.Label()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.ButtonSaveFile = New System.Windows.Forms.Button()
+        Me.RBFileT = New System.Windows.Forms.RadioButton()
+        Me.RBFileF = New System.Windows.Forms.RadioButton()
+        Me.PanelFile = New System.Windows.Forms.Panel()
+        Me.TextBoxWord = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBoxLoc = New System.Windows.Forms.TextBox()
+        Me.TextBoxPron = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxTone = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxRank = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ButtonNext = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.LabelPrev = New System.Windows.Forms.Label()
+        Me.ButtonPrev = New System.Windows.Forms.Button()
+        Me.PanelFile.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ButtonReadFile
+        '
+        Me.ButtonReadFile.Font = New System.Drawing.Font("微软雅黑", 10.0!)
+        Me.ButtonReadFile.Location = New System.Drawing.Point(404, 76)
+        Me.ButtonReadFile.Name = "ButtonReadFile"
+        Me.ButtonReadFile.Size = New System.Drawing.Size(105, 42)
+        Me.ButtonReadFile.TabIndex = 6
+        Me.ButtonReadFile.Text = "修改读入文件"
+        Me.ButtonReadFile.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.DefaultExt = "txt"
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = "文本文件|*.txt"
+        '
+        'LabelFile
+        '
+        Me.LabelFile.AutoSize = True
+        Me.LabelFile.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.LabelFile.Location = New System.Drawing.Point(12, 25)
+        Me.LabelFile.Name = "LabelFile"
+        Me.LabelFile.Size = New System.Drawing.Size(88, 21)
+        Me.LabelFile.TabIndex = 10
+        Me.LabelFile.Text = "生成字典:  "
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.DefaultExt = "txt"
+        Me.SaveFileDialog1.Filter = "文本文件|*.txt"
+        Me.SaveFileDialog1.OverwritePrompt = False
+        '
+        'ButtonSaveFile
+        '
+        Me.ButtonSaveFile.Font = New System.Drawing.Font("微软雅黑", 10.0!)
+        Me.ButtonSaveFile.Location = New System.Drawing.Point(543, 76)
+        Me.ButtonSaveFile.Name = "ButtonSaveFile"
+        Me.ButtonSaveFile.Size = New System.Drawing.Size(105, 42)
+        Me.ButtonSaveFile.TabIndex = 7
+        Me.ButtonSaveFile.Text = "修改输出文件"
+        Me.ButtonSaveFile.UseVisualStyleBackColor = True
+        '
+        'RBFileT
+        '
+        Me.RBFileT.AutoSize = True
+        Me.RBFileT.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.RBFileT.Location = New System.Drawing.Point(20, 3)
+        Me.RBFileT.Name = "RBFileT"
+        Me.RBFileT.Size = New System.Drawing.Size(108, 25)
+        Me.RBFileT.TabIndex = 8
+        Me.RBFileT.Text = "从文件读入"
+        Me.RBFileT.UseVisualStyleBackColor = True
+        '
+        'RBFileF
+        '
+        Me.RBFileF.AutoSize = True
+        Me.RBFileF.Checked = True
+        Me.RBFileF.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.RBFileF.Location = New System.Drawing.Point(159, 3)
+        Me.RBFileF.Name = "RBFileF"
+        Me.RBFileF.Size = New System.Drawing.Size(92, 25)
+        Me.RBFileF.TabIndex = 9
+        Me.RBFileF.TabStop = True
+        Me.RBFileF.Text = "手动输入"
+        Me.RBFileF.UseVisualStyleBackColor = True
+        '
+        'PanelFile
+        '
+        Me.PanelFile.Controls.Add(Me.RBFileT)
+        Me.PanelFile.Controls.Add(Me.RBFileF)
+        Me.PanelFile.Location = New System.Drawing.Point(384, 20)
+        Me.PanelFile.Name = "PanelFile"
+        Me.PanelFile.Size = New System.Drawing.Size(274, 31)
+        Me.PanelFile.TabIndex = 20
+        '
+        'TextBoxWord
+        '
+        Me.TextBoxWord.Font = New System.Drawing.Font("楷体", 25.0!, System.Drawing.FontStyle.Bold)
+        Me.TextBoxWord.Location = New System.Drawing.Point(48, 76)
+        Me.TextBoxWord.Name = "TextBoxWord"
+        Me.TextBoxWord.Size = New System.Drawing.Size(241, 46)
+        Me.TextBoxWord.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.Label1.Location = New System.Drawing.Point(12, 172)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 21)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "位置"
+        '
+        'TextBoxLoc
+        '
+        Me.TextBoxLoc.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.TextBoxLoc.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.TextBoxLoc.Location = New System.Drawing.Point(60, 169)
+        Me.TextBoxLoc.Name = "TextBoxLoc"
+        Me.TextBoxLoc.ShortcutsEnabled = False
+        Me.TextBoxLoc.Size = New System.Drawing.Size(72, 29)
+        Me.TextBoxLoc.TabIndex = 1
+        '
+        'TextBoxPron
+        '
+        Me.TextBoxPron.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.TextBoxPron.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.TextBoxPron.Location = New System.Drawing.Point(234, 169)
+        Me.TextBoxPron.Name = "TextBoxPron"
+        Me.TextBoxPron.ShortcutsEnabled = False
+        Me.TextBoxPron.Size = New System.Drawing.Size(72, 29)
+        Me.TextBoxPron.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(186, 172)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 21)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "发音"
+        '
+        'TextBoxTone
+        '
+        Me.TextBoxTone.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.TextBoxTone.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.TextBoxTone.Location = New System.Drawing.Point(404, 169)
+        Me.TextBoxTone.Name = "TextBoxTone"
+        Me.TextBoxTone.ShortcutsEnabled = False
+        Me.TextBoxTone.Size = New System.Drawing.Size(72, 29)
+        Me.TextBoxTone.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.Label3.Location = New System.Drawing.Point(356, 172)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 21)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "声调"
+        '
+        'TextBoxRank
+        '
+        Me.TextBoxRank.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.TextBoxRank.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.TextBoxRank.Location = New System.Drawing.Point(576, 169)
+        Me.TextBoxRank.Name = "TextBoxRank"
+        Me.TextBoxRank.ShortcutsEnabled = False
+        Me.TextBoxRank.Size = New System.Drawing.Size(72, 29)
+        Me.TextBoxRank.TabIndex = 4
+        Me.TextBoxRank.Text = "1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.Label4.Location = New System.Drawing.Point(528, 172)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 21)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "评级"
+        '
+        'ButtonNext
+        '
+        Me.ButtonNext.Font = New System.Drawing.Font("微软雅黑", 13.0!)
+        Me.ButtonNext.Location = New System.Drawing.Point(485, 230)
+        Me.ButtonNext.Name = "ButtonNext"
+        Me.ButtonNext.Size = New System.Drawing.Size(131, 44)
+        Me.ButtonNext.TabIndex = 21
+        Me.ButtonNext.Text = "下一条"
+        Me.ButtonNext.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.Label5.Location = New System.Drawing.Point(12, 242)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 21)
+        Me.Label5.TabIndex = 22
+        Me.Label5.Text = "上一条:"
+        '
+        'LabelPrev
+        '
+        Me.LabelPrev.AutoSize = True
+        Me.LabelPrev.Font = New System.Drawing.Font("微软雅黑", 12.0!)
+        Me.LabelPrev.Location = New System.Drawing.Point(80, 242)
+        Me.LabelPrev.Name = "LabelPrev"
+        Me.LabelPrev.Size = New System.Drawing.Size(0, 21)
+        Me.LabelPrev.TabIndex = 23
+        '
+        'ButtonPrev
+        '
+        Me.ButtonPrev.Font = New System.Drawing.Font("微软雅黑", 10.0!)
+        Me.ButtonPrev.Location = New System.Drawing.Point(332, 240)
+        Me.ButtonPrev.Name = "ButtonPrev"
+        Me.ButtonPrev.Size = New System.Drawing.Size(76, 31)
+        Me.ButtonPrev.TabIndex = 24
+        Me.ButtonPrev.Text = "上一条"
+        Me.ButtonPrev.UseVisualStyleBackColor = True
+        '
+        'FormMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(680, 298)
+        Me.Controls.Add(Me.ButtonPrev)
+        Me.Controls.Add(Me.LabelPrev)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ButtonNext)
+        Me.Controls.Add(Me.TextBoxRank)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBoxTone)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBoxPron)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxLoc)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxWord)
+        Me.Controls.Add(Me.PanelFile)
+        Me.Controls.Add(Me.ButtonSaveFile)
+        Me.Controls.Add(Me.LabelFile)
+        Me.Controls.Add(Me.ButtonReadFile)
+        Me.Name = "FormMain"
+        Me.Text = "Dictionary Manager"
+        Me.PanelFile.ResumeLayout(False)
+        Me.PanelFile.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonReadFile As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents LabelFile As Label
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents ButtonSaveFile As Button
+    Friend WithEvents RBFileT As RadioButton
+    Friend WithEvents RBFileF As RadioButton
+    Friend WithEvents PanelFile As Panel
+    Friend WithEvents TextBoxWord As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxLoc As TextBox
+    Friend WithEvents TextBoxPron As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxTone As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBoxRank As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ButtonNext As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents LabelPrev As Label
+    Friend WithEvents ButtonPrev As Button
+End Class
---
+++ Dictionary Management/FormMain.vb
@@ -0,0 +1,350 @@
+﻿Imports System.ComponentModel
+
+Public Class FormMain
+    Public x As Integer
+    Public y As Integer
+
+    Dim n As Integer = 0, pn As Integer = 0
+    Dim word() As String
+    Dim pron() As String, ind() As Integer
+    Dim indw As Integer, indp As Integer
+    Dim prevStr As String
+
+    Dim CurrentFileName As String
+    Dim InName, OutName As String
+
+    Dim OutStream As IO.StreamWriter
+
+    Private Sub FormMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        zoomInitialize(Me)
+
+#Region "Initialize directory"
+        OpenFileDialog1.FileName = ""
+        SaveFileDialog1.FileName = "library.txt"
+        OpenFileDialog1.InitialDirectory = Application.StartupPath
+        SaveFileDialog1.InitialDirectory = Application.StartupPath
+        'ButtonReadFile_Click(Nothing, Nothing)
+        InName = "手动输入"
+        ButtonSaveFile_Click(Nothing, Nothing)
+#End Region
+
+    End Sub
+
+    Private Function InitializeReading() As Boolean
+        Dim i As Integer
+        Dim temp As String
+        Dim fstream As IO.StreamWriter
+
+        'Initialize Input file 
+        If Not word Is Nothing Then
+            If Not word(0) = "" And indw <= n Then
+                If Not MsgBox("录入未结束，是否结束本次录入？", MsgBoxStyle.Question & MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                    Return False
+                Else
+                    'Put the remaining words to the current File
+                    If Not prevStr = "" Then
+                        OutStream.WriteLine(prevStr)
+                        prevStr = ""
+                        LabelPrev.Text = ""
+                    End If
+                    fstream = New IO.StreamWriter(CurrentFileName)
+                    If indp > 0 Then
+                        If ind(indp - 1) = indw - 1 Then indp -= 1
+                    End If
+                    For i = indw - 1 To n
+                            temp = word(i)
+                            If ind(indp) = i Then
+                                temp = temp & " " & pron(indp)
+                                indp += 1
+                                If indp > UBound(ind) Then indp -= 1
+                            End If
+                            fstream.WriteLine(temp)
+                        Next
+                        fstream.Close()
+                    End If
+                End If
+        End If
+
+        n = 0 : pn = 0 : indp = 0 : indw = 0
+        ReDim word(0) : ReDim pron(0) : ReDim ind(0)
+        word(0) = ""
+        Return True
+    End Function
+
+#Region "Input Select"
+    Dim DenyChange = False
+    Private Sub RBFileT_CheckedChanged(sender As Object, e As EventArgs) Handles RBFileT.CheckedChanged
+        If RBFileT.Checked Then
+            If Not DenyChange Then
+                ButtonReadFile_Click(Nothing, Nothing)
+                TextBoxWord.ReadOnly = True
+                'Initialize Main
+            Else
+                DenyChange = True
+            End If
+        Else
+            If InitializeReading() Then
+                DenyChange = False
+                InName = "手动输入"
+                'Initialize Main
+                TextBoxWord.ReadOnly = False
+                Refresh_MainLable()
+            Else
+                DenyChange = True
+                RBFileT.Checked = True
+            End If
+        End If
+    End Sub
+#End Region
+
+#Region "Redirect"
+
+    Private Sub ButtonReadFile_Click(sender As Object, e As EventArgs) Handles ButtonReadFile.Click
+        If Not InitializeReading() Then Exit Sub
+
+        OpenFileDialog1.ShowDialog()
+        Debug.Print(OpenFileDialog1.FileName)
+        'check and open sub
+
+#Region "Read Library"
+        Dim fstream As IO.StreamReader
+        Dim temp As String
+        Dim s As Integer, f As Integer
+
+        If IO.File.Exists(OpenFileDialog1.FileName) Then
+            Try
+                fstream = New IO.StreamReader(OpenFileDialog1.FileName)
+                Do While Not fstream.EndOfStream
+                    temp = fstream.ReadLine()
+                    s = 0
+                    Do While Not Asc(temp(s)) < 0
+                        s += 1
+                        If s = Len(temp) Then Exit Do
+                    Loop
+                    If s = Len(temp) Then Continue Do
+                    f = s
+                    Do While Asc(temp(f)) < 0
+                        f += 1
+                        If f = Len(temp) Then Exit Do
+                    Loop
+                    ReDim Preserve word(n)
+                    word(n) = Mid(temp, s + 1, f - s)
+                    'Debug.Print(n & "  " & word(n))
+                    temp = Mid(temp, f + 1)
+                    'Debug.Print(temp)
+                    'Get pronunciation
+                    If Not Trim(temp) Is "" Then
+                        ReDim Preserve pron(pn)
+                        ReDim Preserve ind(pn)
+                        pron(pn) = Trim(temp)
+                        ind(pn) = n
+                        Debug.Print(pron(pn))
+                        pn += 1
+                    End If
+                    n += 1
+                Loop
+                fstream.Close()
+            Catch
+                Debug.Print("ERROR while file reading")
+            End Try
+        End If
+        n -= 1
+        If n = -1 Then
+            MsgBox("This File is empty or error," & vbCrLf & "Please select File again", MsgBoxStyle.Critical)
+            ButtonReadFile_Click(Nothing, Nothing)
+        Else
+            CurrentFileName = OpenFileDialog1.FileName
+
+            Dim t() As String
+            t = Split(CurrentFileName, "\")
+            InName = t(UBound(t))
+            Refresh_LabelFile()
+        End If
+
+#End Region
+
+        Refresh_MainLable()
+
+    End Sub
+
+    Private Sub ButtonSaveFile_Click(sender As Object, e As EventArgs) Handles ButtonSaveFile.Click
+        If Not prevStr = "" Then
+            OutStream.WriteLine(prevStr)
+            prevStr = ""
+            LabelPrev.Text = ""
+        End If
+        Try
+            OutStream.Close()
+        Catch ex As Exception
+
+        End Try
+        SaveFileDialog1.ShowDialog()
+        Debug.Print(SaveFileDialog1.FileName)
+        Dim t() As String
+        t = Split(SaveFileDialog1.FileName, "\")
+        OutName = t(UBound(t))
+        Refresh_LabelFile()
+        'Add select and setting sub
+        If IO.File.Exists(SaveFileDialog1.FileName) Then
+            Dim Append As Boolean
+            If MsgBox("该文件已经存在，是否将输出追加到文件末尾？" & vbCrLf & "（若选择""否""，则原文件将被清空）",
+                   MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then Append = True
+            If Append Then
+                OutStream = New IO.StreamWriter(SaveFileDialog1.FileName, True)
+            Else
+                OutStream = New IO.StreamWriter(SaveFileDialog1.FileName, False)
+            End If
+        End If
+
+        'OutStream.WriteLine("Hello")
+        'OutStream.Close()
+    End Sub
+#End Region
+
+    Private Sub Refresh_MainLable()
+        TextBoxWord.Text = ""
+        TextBoxPron.Text = ""
+        TextBoxTone.Text = ""
+        TextBoxLoc.Text = ""
+        TextBoxRank.Text = "1"
+        TextBoxWord.Focus()
+        If RBFileT.Checked = True Then
+            If indw = n + 1 Then
+                MsgBox("字典生成已全部完成"， MsgBoxStyle.Information)
+                If Not prevStr = "" Then
+                    OutStream.WriteLine(prevStr)
+                    prevStr = ""
+                    LabelPrev.Text = ""
+                    RBFileF.Checked = True
+                End If
+                Refresh_MainLable()
+                Exit Sub
+            End If
+            TextBoxLoc.Focus()
+            TextBoxWord.Text = word(indw)
+            If ind(indp) = indw Then
+                TextBoxPron.Text = pron(indp)
+                indp += 1
+            End If
+            indw += 1
+        End If
+    End Sub
+
+    Private Sub Refresh_LabelFile()
+        LabelFile.Text = String.Format("生成字典：{0,-15} --> {1,-15}", InName, OutName)
+    End Sub
+
+    Private Sub FormMain_SizeChanged(sender As Object, e As EventArgs) Handles Me.SizeChanged
+        setControls(Me.Width / x, Me.Height / y, Me)
+    End Sub
+
+    Private Sub FormMain_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If Not InitializeReading() Then e.Cancel = True
+        If Not prevStr = "" Then
+            OutStream.WriteLine(prevStr)
+            prevStr = ""
+        End If
+        OutStream.Close()
+    End Sub
+
+    Private Sub NextWord(sender As Object, e As EventArgs) Handles ButtonNext.Click
+        Debug.Print("Next")
+        'limit
+        If TextBoxLoc.Text = "" Then Exit Sub
+        If Trim(TextBoxRank.Text) = "" Then TextBoxRank.Text = "1"
+        If Not TextBoxTone.Text = "" And TextBoxPron.Text = "" Then Exit Sub
+        'sort loc
+        Dim loc As String = TextBoxLoc.Text
+        Dim i, j, k As Integer
+        Dim l As Integer = Len(loc) - 1
+        For i = 0 To l
+            k = i
+            For j = i + 1 To l
+                If loc(j) < loc(k) Then k = j
+            Next
+            If k > i Then
+                Debug.Print(Strings.Left(loc, i))
+                loc = Strings.Left(loc, i) & loc(k) & Mid(loc, i + 2, k - i - 1) & loc(i) & Mid(loc, k + 2)
+            End If
+        Next
+        Debug.Print(loc)
+
+        If Not prevStr = "" Then OutStream.WriteLine(prevStr)
+        prevStr = TextBoxWord.Text & ","
+        If Not TextBoxPron.Text = "" Then
+            If TextBoxTone.Text = "" Then TextBoxTone.Text = "0"
+            prevStr = prevStr & TextBoxPron.Text & "," & TextBoxTone.Text & ","
+        End If
+        For i = 0 To Len(loc) - 2
+            prevStr = prevStr & loc(i) & "."
+        Next
+        prevStr = prevStr & loc(Len(loc) - 1) & "," & TextBoxRank.Text
+        'prestr done
+        LabelPrev.Text = prevStr
+        Refresh_MainLable()
+    End Sub
+
+    Private Sub ButtonPrev_Click(sender As Object, e As EventArgs) Handles ButtonPrev.Click
+        If RBFileT.Checked Then
+            If Not prevStr = "" Then
+                If indp > 0 Then
+                    If ind(indp - 1) = indw - 2 Then indp -= 1
+                End If
+                indw -= 2
+                prevStr = ""
+                LabelPrev.Text = ""
+                Refresh_MainLable()
+            End If
+            Else
+            prevStr = ""
+            Refresh_MainLable()
+        End If
+    End Sub
+
+#Region "Input Limit and process"
+    Private Function IsDuplicated(sender As Object, c As Char) As Boolean
+        Dim i As Integer
+        For i = 0 To Len(sender.text) - 1
+            If sender.text(i) = c Then Return True
+        Next
+        Return False
+    End Function
+
+    Private Sub TextBoxLoc_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxLoc.KeyPress
+        'Defult error
+        Dim t As Integer = Asc(e.KeyChar)
+        If t = 8 Then Exit Sub
+        If t = Keys.Enter Then NextWord(Nothing, Nothing)
+        If t < 49 Or t > 57 Then e.Handled = True
+        If Len(sender.text) >= Len(TextBoxWord.Text) Then e.Handled = True
+        If IsDuplicated(sender, e.KeyChar) Then e.Handled = True
+    End Sub
+
+    Private Sub TextBoxPron_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxPron.KeyPress
+        Debug.Print(Asc(e.KeyChar))
+        'Defult nothing
+        Dim t As Integer = Asc(e.KeyChar)
+        If t = 8 Then Exit Sub
+        If t = Keys.Enter Then NextWord(Nothing, Nothing)
+        If t < 65 Or t > 90 And t < 97 Or t > 122 Then e.Handled = True
+    End Sub
+
+    Private Sub TextBoxRank_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxRank.KeyPress
+        'Defult 1
+        Dim t As Integer = Asc(e.KeyChar)
+        If t = 8 Then Exit Sub
+        If t = Keys.Enter Then NextWord(Nothing, Nothing)
+        If t < 49 Or t > 53 Then e.Handled = True
+        If Not sender.text = "" Then e.Handled = True
+    End Sub
+
+    Private Sub TextBoxTone_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxTone.KeyPress
+        'Defult 0
+        Dim t As Integer = Asc(e.KeyChar)
+        If t = 8 Then Exit Sub
+        If t = Keys.Enter Then NextWord(Nothing, Nothing)
+        If t < 48 Or t > 52 Then e.Handled = True
+        If Not sender.text = "" Then e.Handled = True
+    End Sub
+#End Region
+End Class
